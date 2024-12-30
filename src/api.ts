992161da378b54bc92df097a03b7f0a792830070
zelonKim <ksz18601@gmail.com>
2024-12-30 17:57:31 +0900

    deploy3

--- src/api.ts
+++ src/api.ts
@@ -14,7 +14,7 @@ const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/api/v1/"
-      : "https://airbnbclone-tnsp.onrender.com/api/v1",
+      : "https://backend.airbnbzelon.xyz/api/v1/",
   withCredentials: true,
 });
 
